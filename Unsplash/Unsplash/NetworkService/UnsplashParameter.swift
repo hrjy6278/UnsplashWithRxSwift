070446b214fdad5f1159a6c85a4dea75643243cb
hrjy6278 <hrjy6278@naver.com>
2022-02-14 12:04:42 +0900

    chore: Clinet ID, secret ID 숨김처리
    - gitignore에 xcconfig 확장자명 추가

--- Unsplash/Unsplash/NetworkService/UnsplashParameter.swift
+++ Unsplash/Unsplash/NetworkService/UnsplashParameter.swift
@@ -16,8 +16,8 @@ enum UnsplashParameter {
         case wirteUser = "write_user"
     }
     
-    static let clientID = "UUGSJm9EzJM5tw1ngfNkKSn_OGW26g-O-z5FgLJEPPk"
-    static let clientSecret = "zXsRvwnvw_55_XssA86y3ow-zqF0PIkOgzobdN5zewA"
+    static let clientID = Bundle.main.infoDictionary?["UNSPLASH_CLIENT_KEY"] as? String ?? ""
+    static let clientSecret = Bundle.main.infoDictionary?["UNSPLASH_SECERET_KEY"] as? String ?? ""
     static let redirectURL = "jissCallback://"
     static let responseType = "code"
     static let scope = Scope.allCases
